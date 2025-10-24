6415a56253441b608809d75fe6710a53437a2aec
charith-codex <pvm.charith@gmail.com>
2025-10-24 16:49:07 +0530

    add sheets and set as mobile responsive header

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -1,9 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import { APP_NAME } from "@/lib/constants";
-import { Button } from "@/components/ui/button";
-import { UserIcon } from "lucide-react";
-import ModeToggle from "./mode-toggle";
+import Menu from "./menu";
 
 const Header = () => {
   return (
@@ -23,14 +21,7 @@ const Header = () => {
             </span>
           </Link>
         </div>
-        <div className="space-x-2">
-          <ModeToggle />
-          <Button asChild>
-            <Link href="/cart">
-              <UserIcon /> Sign In
-            </Link>
-          </Button>
-        </div>
+        <Menu />
       </div>
     </header>
   );
---
+++ components/shared/header/menu.tsx
@@ -0,0 +1,58 @@
+import { Button } from "@/components/ui/button";
+import ModeToggle from "./mode-toggle";
+import Link from "next/link";
+import { EllipsisVertical, UserIcon } from "lucide-react";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+
+const Menu = () => {
+  return (
+    <div className="flex justify-end gap-3">
+      <nav className="hidden md:flex w-full max-w-xs gap-3">
+        <ModeToggle />
+        <Button asChild>
+          <Link href="/dashboard">Dashboard</Link>
+        </Button>
+
+        <Button asChild>
+          <Link href="/cart">
+            <UserIcon /> Sign In
+          </Link>
+        </Button>
+      </nav>
+      <nav className="md:hidden">
+        <Sheet>
+          <SheetTrigger className="align-middle">
+            <EllipsisVertical />
+          </SheetTrigger>
+          <SheetContent className="flex flex-col p-5">
+            <SheetTitle>Menu</SheetTitle>
+            <ModeToggle />
+            <Button asChild>
+              <Link href="/dashboard">Dashboard</Link>
+            </Button>
+            <Button asChild>
+              <Link href="/lms">LMS</Link>
+            </Button>
+            <Button asChild>
+              <Link href="/lms-cms">CMS</Link>
+            </Button>
+            <Button asChild>
+              <Link href="/cart">
+                <UserIcon /> Sign In
+              </Link>
+            </Button>
+            <SheetDescription></SheetDescription>
+          </SheetContent>
+        </Sheet>
+      </nav>
+    </div>
+  );
+};
+
+export default Menu;
