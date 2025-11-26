89226f50f8f28873cbe6cd807794e98890ce9eb3
charith-codex <pvmcharith@gmail.com>
2025-11-26 14:13:32 +0530

    remove google calendar

--- app/(testing)/test/calendar/page.tsx
+++
@@ -1,9 +0,0 @@
-import CalendarEmbed from "@/components/CalendarEmbed";
-
-export default function EventsPage() {
-  return (
-    <main className="min-h-screen bg-gray-50">
-      <CalendarEmbed />
-    </main>
-  );
-}
