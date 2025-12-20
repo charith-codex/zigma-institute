d5d1e5813899842f857aa0b1ff202d54e36c8e87
charith-codex <pvmcharith@gmail.com>
2025-12-20 23:20:39 +0530

    feat: Initialize Prisma client with Neon adapter for database connectivity.

--- db/prisma.ts
+++ db/prisma.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from "../lib/generated/prisma/client";
 import { PrismaNeon } from "@prisma/adapter-neon";
 import dotenv from "dotenv";
 
-dotenv.config();
+dotenv.config({ quiet: true });
 const connectionString = `${process.env.DATABASE_URL}`;
 const adapter = new PrismaNeon({ connectionString });
 
